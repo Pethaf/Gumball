cf1258604e09ad2e8bcdaa35f69a35570438247a
Pethaf <Peter Hafid>
2017-04-08 19:24:09 +0200

    Fixes

--- mightygumball.js
+++ mightygumball.js
@@ -1,4 +1,8 @@
-// window.onload = function() { Deprecated, using JSONP instead 
+ window.onload = function() { 
+ 	setInterval(handleRefresh,10000);
+}
+var lastreporttime = 0;
+//Deprecated, using JSONP instead 
 // 	var url = "http://localhost:8000/sales.json"
 // 	var request = new XMLHttpRequest(); 
 // 	request.onreadystatechange = function() 
@@ -22,4 +26,22 @@ function updateSales(salesObject)
 	 	event.innerHTML = element.name + " sold " + element["sales"] + " gumballs "; 
 	 	salesDiv.appendChild(event);
 	 });
+}
+
+function handleRefresh(callBack = function() { alert("I'm alive");})
+{
+	var url = "http://gumball.wickedlysmart.com?callback=updateSales&lastreporttime="+ (new Date()).getTime();
+	var scriptElem = document.createElement("script");
+	scriptElem.setAttribute("src",url);
+	scriptElem.setAttribute("id","jsonp");
+	var oldScriptElem = document.getElementById("jsonp");
+	var head = document.getElementsByTagName("head")[0];
+	if(oldScriptElem == null)
+	{
+		head.appendChild(scriptElem);
+	}
+	else
+	{
+		head.replaceChild(scriptElem, oldScriptElem);
+	}
 }
