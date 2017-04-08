1d12169f05a86ebb794234f5af98e05724d563c6
Pethaf <Peter Hafid>
2017-04-08 17:59:26 +0200

    Updated to JSONP

--- mightygumball.js
+++ mightygumball.js
@@ -1,25 +1,25 @@
-window.onload = function() {
-	var url = "http://localhost:8000/sales.json"
-	var request = new XMLHttpRequest(); 
-	request.onreadystatechange = function() 
-	{
-		if (request.readyState == 4 && request.status == 200)
-		{
-			updateSales(request.responseText);
-		}
-	};
-	request.open("GET",url);
-	request.send(null);
-}
+// window.onload = function() { Deprecated, using JSONP instead 
+// 	var url = "http://localhost:8000/sales.json"
+// 	var request = new XMLHttpRequest(); 
+// 	request.onreadystatechange = function() 
+// 	{
+// 		if (request.readyState == 4 && request.status == 200)
+// 		{
+// 			updateSales(request.responseText);
+// 		}
+// 	};
+// 	request.open("GET",url);
+// 	request.send(null);
+// }
 
-function updateSales(salesItem)
+function updateSales(salesObject)
 {	
 	var salesDiv = document.getElementById("sales");
-	var sales = JSON.parse(salesItem);
-	sales.forEach(function(element) {
+	salesObject.forEach(function(element) 
+	{
 		var event = document.createElement("div");
-		event.setAttribute("class", "salesItem color-secondary-1-0");
-		event.innerHTML = element.name + " sold " + element["sales"] + " gumballs "; 
-		salesDiv.appendChild(event);
-	});
+	 	event.setAttribute("class", "salesItem color-secondary-1-0");
+	 	event.innerHTML = element.name + " sold " + element["sales"] + " gumballs "; 
+	 	salesDiv.appendChild(event);
+	 });
 }
